d891cad94e17e883c2965d0fe37f151a487e45fe
fujun-iri <fujun-iri@360.cn>
2017-03-29 19:54:39 +0800

    update code

--- index.js
+++ index.js
@@ -1,77 +1,99 @@
-;(function () {
+module.exports = {
+    version: '1.0.0',
+    install: installVFlowGrid
+};
 
-  var vFlowGrid = {}
+function installVFlowGrid(Vue, options) {
+    const INIT_FLAG = 'vflowgrid';
 
-  vFlowGrid.install = function (Vue, options) {
+    /**
+     * this will decide whether to update DOM
+     */
+    const shouldUpdate = (el, binding, vnode, oldVnode) => {
+        const initialized     = () => !!el.getAttribute(INIT_FLAG);
+        const bindingChanged  = () => binding.value !== binding.oldValue;
+        const childrenChanged = () => vnode.children.length !== oldVnode.children.length;
+        return !initialized() || bindingChanged() || childrenChanged();
+    };
+
+    /**
+     * calculate appropriate index of the column to which
+     * we should append the next item
+     */
+    const getAppendCol = function (columns) {
+        let minH = columns[0].offsetHeight;
+        return columns.slice(1).reduce((accu, col, i) => {
+            if (col.offsetHeight < minH) {
+                minH = col.offsetHeight;
+                accu = i + 1;
+            }
+            return accu;
+        }, 0);
+    };
+
+    /**
+     * add an element for clearfix
+     */
+    const clearfix = (el) => {
+        const cf = document.createElement('i');
+        const style = cf.style;
+        style.clear = 'both';
+        style.visibility = 'hidden';
+        style.width = 0;
+        style.height = 0;
+        return cf;
+    };
+
     Vue.directive('flowgrid', {
-        componentUpdated: function (el, binding, vnode) {
-            options = options || {}
+        /**
+         * hide each item as soon as they are inserted
+         * to avoid flash of content.
+         * here we use `visibility: hidden` instead of
+         * `display: none`, because offsetHeight of items
+         * are required when calculating
+         */
+        inserted: function (el, binding, vnode) {
+            Array.from(el.children).forEach(li => li.style.visibility = 'hidden');
+        },
 
-            //只运行一次
-            if (el.querySelector("#vFlowgrid1")) {
+        /**
+         * on update
+         */
+        componentUpdated: function (el, binding, vnode, oldVnode) {
+            // avoid unneccesary updates
+            if (!shouldUpdate(el, binding, vnode, oldVnode)) {
                 return;
             }
 
-            //瀑布流要展示的项目
-            var items = [];
-            while(el.childNodes.length) {
-                items.unshift(el.removeChild(el.childNodes[--el.childNodes.length]));
-            }
-            var itemCount = items.length;
-            //瀑布流及数目
-            var grids = [];
-            var gridCount = binding.value;
+            // grids
+            const items = vnode.children.map(i => i.elm);
 
-            //插入gridCount数量的瀑布
-            for (var i=0; i<gridCount; i++) {
-                var grid = document.createElement("div");
-                grid.id = "vFlowgrid" + (i+1);
-                grid.style.width = 100/gridCount + '%';
-                grid.style.float = 'left';
-                grids.push(grid);
-                el.appendChild(grid);
-            }
+            // colums
+            const colNum  = binding.value;
+            const columns = Array.from({ length: colNum }, () => {
+                const col = document.createElement('div');
+                col.style.width = `${100 / colNum}%`;
+                col.style.float = 'left';
+                return col;
+            });
 
-            //获取下一个需要插入项目的瀑布
-            function getNextAppendGrid() {
-                var smallest = el.childNodes[0].offsetHeight;
-                var smallestIndex = 0;
-                Array.prototype.forEach.call(el.childNodes, (grid, index) => {
-                    if(grid.offsetHeight < smallest) {
-                        smallest = grid.offsetHeight;
-                        smallestIndex = index;
-                    }
-                });
-                return smallestIndex;
-            }
+            // mark `el` as initialized
+            el.setAttribute(INIT_FLAG, true);
 
-            //按顺序插入所有item
-            for (var i=0; i<itemCount; i++) {
-                if (i<gridCount) {
-                    el.childNodes[i].appendChild(items[i]);
-                } else {
-                    var appendGridIndex = getNextAppendGrid();
-                    el.childNodes[appendGridIndex].appendChild(items[i]);
-                }
-            }
+            // remove elements first
+            Array.from(el.children).map(item => el.removeChild(item));
 
-            //清除浮动
-            var clearfix = document.createElement("div");
-            clearfix.style.clear = 'both';
-            el.appendChild(clearfix);
+            // append col to element
+            columns.map(c => el.appendChild(c));
+
+            // start to append items
+            items.forEach((item, i) => {
+                columns[i < colNum ? i : getAppendCol(columns)].appendChild(item);
+                item.style.visibility = 'visible';
+            });
+
+            // clearfix
+            el.appendChild(clearfix());
         }
-    })
-  }
-
-  if (typeof exports == "object") {
-    module.exports = vFlowGrid
-  } else if (typeof define == "function" && define.amd) {
-    define([], function () {
-      return vFlowGrid
-    })
-  } else if (window.Vue) {
-    window.vFlowGrid = vFlowGrid
-    Vue.use(vFlowGrid)
-  }
-
-})()
+    });
+}
